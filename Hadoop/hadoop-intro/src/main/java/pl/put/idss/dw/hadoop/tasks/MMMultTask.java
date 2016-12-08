8919a4d736e4be4a7a816d3ac4653ecf8fe3625d
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-12-08 21:49:50 +0100

    What the fuck is wrong

--- Hadoop/hadoop-intro/src/main/java/pl/put/idss/dw/hadoop/tasks/MMMultTask.java
+++ Hadoop/hadoop-intro/src/main/java/pl/put/idss/dw/hadoop/tasks/MMMultTask.java
@@ -55,7 +55,6 @@ public class MMMultTask {
 		@Override
 		public void reduce(Text key, Iterable<Text> values,
 				Context context) throws IOException, InterruptedException {
-			Set<Text> newMatrix = new HashSet<>(); 
 			ArrayList<Text> mArray = new ArrayList<>(); 
 			ArrayList<Text> nArray = new ArrayList<>(); 
 			String outputs = "";  
@@ -66,20 +65,37 @@ public class MMMultTask {
 				int cellValue = Integer.parseInt(stringValue[2]);
 				if(stringValue[0].equals("M")) {
 					mArray.add(value);
-				} else {
+				} else if(stringValue[0].equals("N")) {
 					nArray.add(value);
+				}
+			}
+			
+			for(int i=0; i<mArray.size(); i++) {
+				System.out.println(mArray.get(i));
+				for(int j=0; j<nArray.size(); j++) {
+					System.out.println(nArray.get(j));
+					String[] mStrings = mArray.get(i).toString().split("\\s"); 
+					String[] nStrings = nArray.get(j).toString().split("\\s"); 
+					int value = Integer.parseInt(mStrings[2])*Integer.parseInt(nStrings[2]); 
+//					System.out.println(mStrings[1]);
+//					System.out.println(nStrings[1]);
 				}
 			}
 			for(Text mValue: mArray) {
 				for(Text nValue: nArray) {
+//					System.out.println(mValue);
+//					System.out.println(nValue);
 					String[] mStrings = mValue.toString().split("\\s"); 
 					String[] nStrings = nValue.toString().split("\\s"); 
 					int value = Integer.parseInt(mStrings[2])*Integer.parseInt(nStrings[2]); 
+//					System.out.println(mStrings[1]);
+//					System.out.println(nStrings[1]);
 					outputs = outputs + mStrings[1] + "," + nStrings[1] + "," + String.valueOf(value)+" "; 
 				}
 			}
 			outputs = outputs.substring(0, outputs.length()-1); 
 			context.write(key, new Text(outputs));
+			outputs = "";
 		}
 	}
 
@@ -107,6 +123,7 @@ public class MMMultTask {
 		@Override
 		public void reduce(Text key, Iterable<IntWritable> values,
 				Context context) throws IOException, InterruptedException {
+			result.set(0);
 			int sum=0; 
 			for(IntWritable element:values){
 				sum += element.get(); 
